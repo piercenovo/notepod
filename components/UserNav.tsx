0aa07914d21d21060d1e9178a081e2c93498b9f3
piercenovo <piercenovoah@gmail.com>
2024-03-16 22:06:16 -0500

    fix: add single quotes to ui components

--- components/UserNav.tsx
+++ components/UserNav.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { CreditCard, DoorClosed, Home, Settings } from 'lucide-react'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
